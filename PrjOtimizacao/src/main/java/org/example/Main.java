a4854f0040931b817dc1ee43dd71409041876b87
Marielle Vaz <118058661+marielle-vaz@users.noreply.github.com>
2024-10-11 19:27:23 -0300

    Add files via upload

---
+++ PrjOtimizacao/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.net.StandardSocketOptions;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Util u = new Util();
+
+        int coeficientes[][] = {
+                {1, -3, -2, 0, 0, 0, 0},
+                {0,  2,  1, 1, 0, 0, 100},
+                {0,  1,  1, 0, 1, 0, 80},
+                {0,  1,  0, 0, 0, 1, 40}
+        };
+
+        String variaveisBasicas[] = {"z", "f1", "f2", "f3"};
+
+        String variaveis[] = {"z", "x1", "x2", "f1", "f2", "f3"};
+
+
+
+        int coluna = u.validaFuncaoOjetivo(coeficientes);
+
+        if(coluna == 0){
+            System.out.println("A matriz está otimizada!");
+        } else {
+            System.out.println("A coluna que possui o menor número é: " + coluna);
+        }
+
+        int linhaPivo = u.obtemLinhaPivo(coluna, coeficientes);
+
+        System.out.println(linhaPivo);
+
+    }
+
+    /*
+    @Override
+    public String toString() {
+        return  "coeficientes=" + Arrays.deepToString(coeficientes) +
+                "\nvariaveisBasicas=" + Arrays.toString(variaveisBasicas) +
+                "\nvariaveis=" + Arrays.toString(variaveis);
+    }*/
+
+}
---
+++ PrjOtimizacao/src/main/java/org/example/Util.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Util {
+
+    public int validaFuncaoOjetivo(int coeficientes[][]){
+        int menor = 0, c = 0;
+
+        for(int coluna = 0; coluna < 7; coluna++){
+            if(coeficientes[0][coluna] <= menor){
+                menor = coeficientes[0][coluna];
+                c = coluna;
+            }
+        }
+
+        return c;
+    }
+
+    public int obtemLinhaPivo(int coluna, int coeficientes[][]){
+        int linha = 0, l = 0, menor = 100000;
+
+        for(linha = 1; linha < 4; linha++){
+            coeficientes[linha][6] = coeficientes[linha][6] / coeficientes[linha][coluna];
+            System.out.println(coeficientes[linha][6]);
+
+            if(coeficientes[linha][6] <= menor){
+                menor = coeficientes[linha][coluna];
+                l = linha;
+            }
+        }
+
+        return l;
+
+    }
+
+
+}
